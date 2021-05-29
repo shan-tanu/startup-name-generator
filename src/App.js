d101d94a9ba6ecd8274ec24186e0feb67252d067
Shantanu M G <shantanumg1999@gmail.com>
2021-05-29 16:40:51 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Deploying App<br>
-          <p>This is so cool!!</p>
+        Deploying App<br />
+        <p>This is so cool!!</p>
       </header>
     </div>
   );
